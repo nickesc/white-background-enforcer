02f16fa4ee7a2fb5e96494b8823feeb5205a4a6d
N. Escobar <nick@nescobar.media>
2025-02-18 17:13:19 -0500

    refactored and added custom property to identify if bg is from wbge

--- src/wbge.js
+++ src/wbge.js
@@ -1,25 +1,34 @@
 function setBackgroundColor(item) {
+    // Get body style and bg color
+    const bodyStyle = window.getComputedStyle(document.body)
+    const currentBgColor = bodyStyle.backgroundColor;
 
     // only act if Enabled in settings
-    if (item.wbgeEnabled != false){
-        // Get background color of body
-        const bodyStyle = window.getComputedStyle(document.body)
-        const currentBgColor = bodyStyle.backgroundColor;
-
-        // Check if background-color is transparent or unset
-        if (!currentBgColor || currentBgColor === 'rgba(0, 0, 0, 0)' || currentBgColor === 'transparent') {
-            let color = "white";
-            if (item.wbgeColor) {
-                color = item.wbgeColor;
-            }
-            document.body.style.backgroundColor = color;
+    if (item.wbgeEnabled == false){
+        if (bodyStyle.getPropertyValue("--wbge-status")==="enabled") {
+            document.body.style.backgroundColor = "unset";
+            document.body.style.setProperty("--wbge-status", "disabled");
+            console.log("WBGE Disabled")
         }
+        return;
     }
 
+    // Set default bg color
+    let color = "white";
+    if (item.wbgeColor) {
+        color = item.wbgeColor;
+    }
+
+    // Check if background-color is transparent or unset
+    if (!currentBgColor || currentBgColor === 'rgba(0, 0, 0, 0)' || currentBgColor === 'transparent') {
+        document.body.style.backgroundColor = color;
+        document.body.style.setProperty("--wbge-status", "enabled");
+        console.log("WBGE Executed")
+    }
 }
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+    console.error(`Error: ${error}`);
 }
 
 function wbge() {
